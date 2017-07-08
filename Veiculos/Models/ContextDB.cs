b92e9164973e134d9efbcf939d9f35402540a161
Arthur Fritz <arthurfritzz@gmail.com>
2017-07-08 11:25:47 -0300

    Mapeamento de entidades, e criação do contextoBD

---
+++ Veiculos/Models/ContextDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos.Models
+{
+    class ContextoDb : DbContext
+    {
+        public ContextoDb() : base("DefaultConnection"){}
+
+        public DbSet<PessoaModel> Pessoa { get; set; }
+        public DbSet<FotoModel> Foto { get; set; }
+        public DbSet<AssinaturaModel> Assinatura { get; set; }
+        public DbSet<MultasModel> Multa { get; set; }
+        public DbSet<VeiculoModel> Veiculo { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
